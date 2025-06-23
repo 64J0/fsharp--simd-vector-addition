58b6b15f42286afe85549e439f305e52ee936714
64J0 <vinigaio97@gmail.com>
2025-06-23 10:08:00 -0300

    improve comments

--- src/Sse.fs
+++ src/Sse.fs
@@ -16,6 +16,10 @@ let sseAdd (a: float32[]) (b: float32[]) =
     let result = Array.zeroCreate<float32> len
     let simdWidth = Vector128<float32>.Count // 4 for SSE
 
+    // Span<T> provides a type-safe and memory-safe representation of a contiguous region of arbitrary memory.
+    // At first it seems similar to using arrays, but it allows for more efficient memory access patterns.
+    // More information:
+    // https://learn.microsoft.com/en-us/archive/msdn-magazine/2018/january/csharp-all-about-span-exploring-a-new-net-mainstay
     let spanA = a.AsSpan()
     let spanB = b.AsSpan()
     let spanR = result.AsSpan()
@@ -23,12 +27,17 @@ let sseAdd (a: float32[]) (b: float32[]) =
     let mutable i = 0
 
     while i <= len - simdWidth do
-        // Get 4-element slices and cast them to bytes
+        // This MemoryMarshal.Cast is used to convert the Span<float32> to a Span<Vector128<float32>>.
+        // A Span<float32> is a contiguous region of memory that can be used to represent an array or a 
+        // portion of an array. Since it has this float32 type, our program knows that each element takes 
+        // 4 bytes, and with this information it knows how to slice it properly.
         let va =
             MemoryMarshal.Cast<float32, Vector128<float32>>(spanA.Slice(i, simdWidth)).[0]
+        // val va: Vector128<float32> = <a_{i}, a_{i+1}, a_{i+2}, a_{i+3}>
 
         let vb =
             MemoryMarshal.Cast<float32, Vector128<float32>>(spanB.Slice(i, simdWidth)).[0]
+        // val vb: Vector128<float32> = <b_{i}, b_{i+1}, b_{i+2}, b_{i+3}>
 
         let vsum = Sse.Add(va, vb)
         MemoryMarshal.Cast<float32, Vector128<float32>>(spanR.Slice(i, simdWidth)).[0] <- vsum
